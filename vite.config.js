4cbc6db556874406f893b47bd2be41347c656e17
guash12 <safaricomagent711@gmail.com>
2026-08-03 22:10:54 +0300

    Fix Vercel routing and base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/MyPortfolioWeb/'  // important for GitHub Pages
-  
+  base: process.env.VERCEL ? '/' : '/MyPortfolioWeb/',
 })
